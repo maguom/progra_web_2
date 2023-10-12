112b7f28df2d54944ce0ce5d64346eb8f60c7406
maguom <114527521+maguom@users.noreply.github.com>
2023-10-12 21:52:29 +0000

    cambio en el archivo index.html

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Función para verificar si un número es primo
 function esPrimo(numero) {
     if (numero <= 1) {
-      return false;
+      return false
     }
     for (var i = 2; i < numero; i++) {
       if (numero % i === 0) {
-        return false;
+        return false
       }
     }
     return true;
@@ -13,23 +13,23 @@ function esPrimo(numero) {
 
   // Función para imprimir los primeros "n" números primos
   function imprimirNPrimos(n) {
-    var primosEncontrados = 0;
-    var numeroActual = 2; // Comenzamos con el primer número primo
+    var primosEncontrados = 0
+    var numeroActual = 2 // Comenzamos con el primer número primo
 
     while (primosEncontrados < n) {
       if (esPrimo(numeroActual)) {
-        document.write(numeroActual + " ");
-        primosEncontrados++;
+        document.write(numeroActual + " ")
+        primosEncontrados++
       }
-      numeroActual++;
+      numeroActual++
     }
   }
 
-  var n = parseInt(prompt("Ingrese la cantidad de números primos que desea imprimir"));
+  var n = parseInt(prompt("Ingrese la cantidad de números primos que desea imprimir"))
 
   if (!isNaN(n) && n > 0) {
-    document.write("Los primeros " + n + " números primos son: ");
-    imprimirNPrimos(n);
+    document.write("Los primeros " + n + " números primos son: ")
+    imprimirNPrimos(n)
   } else {
-    document.write("Por favor, ingrese una cantidad válida de números primos.");
+    document.write("Por favor, ingrese una cantidad válida de números primos.")
   }
